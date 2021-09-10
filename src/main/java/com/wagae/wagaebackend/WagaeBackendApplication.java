601537ff74798c6bd05d0ae435f2a8978e461999
Jaewook Choi <eari100@naver.com>
2021-09-10 23:41:57 +0900

    first commit

---
+++ src/main/java/com/wagae/wagaebackend/WagaeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wagae.wagaebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagaeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WagaeBackendApplication.class, args);
+    }
+
+}
