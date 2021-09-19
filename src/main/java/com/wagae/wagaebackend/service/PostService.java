6348c9564ffc9f429bcc6b80da9fbcf3fe11445a
Jaewook <eari100@naver.com>
2021-09-19 12:15:14 +0900

    feat: PostService 생성
    - findAll() 메서드 추가

---
+++ src/main/java/com/wagae/wagaebackend/service/PostService.java
@@ -0,0 +1,23 @@
+package com.wagae.wagaebackend.service;
+
+import com.wagae.wagaebackend.domain.post.PostRepository;
+import com.wagae.wagaebackend.web.dto.PostListResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    @Transactional(readOnly = true)
+    public List<PostListResponseDto> findAll() {
+        return postRepository.findAll().stream()
+                .map(PostListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
